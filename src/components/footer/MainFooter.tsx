b2985dc4a11978a260f82af749d75423ad2ddf16
Andre <adsf@poli.br>
2023-05-25 21:27:56 -0300

    testing flexbox

---
+++ src/components/footer/MainFooter.tsx
@@ -0,0 +1,23 @@
+export default function MainFooter() {
+  return (
+    <>
+      <div className="flex flex-wrap justify-around flex-row gap-6 grid-cols-3">
+        <div className="bg-white p-5 w-48 h-48">
+          <span>w4234234</span>
+        </div>
+        <div className="bg-white p-5 w-48 h-48">
+          <span>2342342342342342</span>
+        </div>
+        <div className="bg-white p-5 w-48 h-48">
+          <span>234234</span>
+        </div>
+        <div className="bg-white p-5 w-48 h-48">
+          <span>243234234234234</span>
+        </div>
+        <div className="bg-white p-5 w-48 h-48">
+          <span>234234234</span>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import Link from "next/link"
+import Link from "next/link";
+import MainFooter from "@/components/footer/MainFooter";
 
-export default function Home(){
+export default function Home() {
   return (
     <div>
-    <h1> hello world</h1>
-    <Link href='/posts'> 
-      Posts
-    </Link>
+      <h1> hello world</h1>
+      <Link href="/posts">Posts</Link>
+      <MainFooter></MainFooter>
     </div>
-  )
-  
+  );
 }
