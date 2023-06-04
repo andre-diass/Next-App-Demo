d4ce1dd55401ba64f387479e56961210edc02e61
Andre <adsf@poli.br>
2023-06-04 18:36:58 -0300

    delete request

--- src/pages/getRequest.tsx
+++ src/pages/getRequest.tsx
@@ -1,24 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-interface Message {
-  name: string;
+interface Comment {
+  id: number;
+  text: string;
 }
 
 export default function TesteAPI() {
-  const [message, setMessage] = useState<Message>({ name: "" });
+  const [message, setMessage] = useState<Comment[]>([]);
 
   const fetchMessage = async () => {
     const response = await fetch("/api/hello");
     const data = await response.json();
     console.log(data);
     setMessage(data);
-    
+  };
+
+  const deleteComment = async (commentId: number) => {
+    const response = await fetch(`/api/comments/${commentId}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+      fetchMessage(); // Fetch comments again after successful deletion
+    }
   };
 
   return (
     <>
       <button onClick={fetchMessage}> Load comments</button>
-      <h1>{message.name}</h1>
+      {message.map((comment) => (
+        <div key={comment.id}>
+          {comment.id}. {comment.text}
+          <button onClick={() => deleteComment(comment.id)}>Delete</button>
+        </div>
+      ))}
     </>
   );
 }
