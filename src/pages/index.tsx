56fe5294b091173580e1cacbfe6e7369effc8eef
Andre <adsf@poli.br>
2023-06-06 11:28:21 -0300

    next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1> hello world</h1>
-      <Link href="/posts">Posts</Link>
-      <MainFooter></MainFooter>
+    
     </div>
   );
 }
